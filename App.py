44af0d33c01af7972440fe19044f051b07158632
mariana42003 <mariiiiduarte@gmail.com>
2026-02-13 17:14:00 -0500

    Implementa suma mayor

--- App.py
+++ App.py
@@ -1,5 +1,8 @@
 def analizar(Lista):
-    pass;
+    suma=sum(Lista)
+    mayor=max(Lista)
+    return{"suma": suma ,
+    "mayor": mayor }
     if __name__=="__Main__":
         numeros = [4,8,2,10]
         resultado = analizar(numeros)

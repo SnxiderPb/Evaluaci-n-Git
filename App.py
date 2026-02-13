d7e6d87a15070befc616f7c873644d96c677293e
Develpjuan <romeoquintero0@gmail.com>
2026-02-13 17:16:13 -0500

    Implementa Menor Y Promedio

--- App.py
+++ App.py
@@ -1,5 +1,10 @@
 def analizar(Lista):
-    pass;
+    menor = min (mixta);
+    promedio = sum (mixta)/len(mixta)
+    return {
+        "Menor":menor,
+        "Promedio":sum(Lista)
+    }
     if __name__=="__Main__":
         numeros = [4,8,2,10]
         resultado = analizar(numeros)
